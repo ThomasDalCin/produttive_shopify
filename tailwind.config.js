124de6208fbc3a19319328895e9343d39b2d422c
Thomas Dal Cin <tdalcin96@gmail.com>
2024-07-10 15:46:34 +0200

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,6 +48,24 @@ module.exports = {
         '80-percent': '80%',
         '90-percent': '90%'
       },
+      minWidth: {
+        '200': '200px',
+        '300': '300px',
+        '400': '400px',
+        '500': '500px',
+        '600': '600px',
+        '700': '700px',
+        '800': '800px',
+        '10-percent': '10%',
+        '20-percent': '20%',
+        '30-percent': '30%',
+        '40-percent': '40%',
+        '50-percent': '50%',
+        '60-percent': '60%',
+        '70-percent': '70%',
+        '80-percent': '80%',
+        '90-percent': '90%'
+      },
       screens: {
         'xs': '435px',
       },
